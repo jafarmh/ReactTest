d76bf6db36b061c4e0ebb99138b468a4e2cbd972
jafarmh <jafar.m@yahoo.com>
2019-07-18 13:38:00 +0430

    Add files via upload

---
+++ build/precache-manifest.42162c8d2d1811bf6157684f8877e501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d3884b0c7108d5207b3586957b0521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f13fcf5a197249f80b8",
+    "url": "/static/css/main.710cd7bf.chunk.css"
+  },
+  {
+    "revision": "b4687835b21ec167378f",
+    "url": "/static/js/2.a8451648.chunk.js"
+  },
+  {
+    "revision": "9f13fcf5a197249f80b8",
+    "url": "/static/js/main.0d9dc480.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
